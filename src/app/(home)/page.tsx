46d207642b7269ea1b0d8d84615ca0c537a8fc31
pnodet <paul.nodet@gmail.com>
2023-11-30 12:39:54 +0700

    feat: add home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,73 @@
+import Image from "next/image";
+import Link from "next/link";
+import HeroAssetImage from "~/hero-asset.png";
+import HeroGradientImage from "~/hero-gradient.png";
+import styles from "./home.module.css";
+
+export default function HomePage() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.hero}>
+          <div className={styles.heroBackground}>
+            <div className={styles.heroBackgroundInner}>
+              <Image
+                src={HeroGradientImage}
+                width={1390}
+                height={1390}
+                alt="Background gradient from red to blue"
+                quality={100}
+                className={styles.gradient}
+              />
+            </div>
+          </div>
+          <div className={styles.heroAssetFrame}>
+            <Image
+              src={HeroAssetImage}
+              width={860}
+              height={540}
+              alt="Hero asset, NFT marketplace"
+              quality={100}
+              className={styles.heroAsset}
+            />
+          </div>
+          <div className={styles.heroBodyContainer}>
+            <div className={styles.heroBody}>
+              <h1 className={styles.heroTitle}>
+                <span className={styles.heroTitleGradient}>
+                  Build NFT Marketplaces
+                </span>
+                <br />
+                faster than ever.
+              </h1>
+              <p className={styles.heroSubtitle}>
+                <Link
+                  className={styles.link}
+                  href="https://thirdweb.com"
+                  target="_blank"
+                >
+                  thirdweb
+                </Link>{" "}
+                gives you the tools you need to create audited, performant, and
+                flexible NFT marketplaces in <b>hours</b>, <i>not months</i>.
+              </p>
+
+              <div className={styles.heroCtaContainer}>
+                <Link className={styles.heroCta} href="/buy">
+                  Get Started
+                </Link>
+                <Link
+                  className={styles.secondaryCta}
+                  href="https://github.com/thirdweb-example/marketplace-v3"
+                  target="_blank"
+                >
+                  GitHub
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
